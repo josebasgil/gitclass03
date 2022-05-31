2edd103cce7a97c55bc1979da8ba1c0197771869
josebasgil <josebasgil29@gmail.com>
2022-05-31 18:36:43 -0500

    1st commit

---
+++ src/GitHub/GitAndGithub.java
@@ -0,0 +1,12 @@
+package GitHub;
+
+public class GitAndGithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		System.out.println("today we will learn github");
+		
+	}
+
+}
